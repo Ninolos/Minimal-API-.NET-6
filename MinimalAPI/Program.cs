fde59dc0a70a9365cc8ae68605e89943782a7c4a
Jonathan Daniel Loth <ninoloth@gmail.com>
2022-03-28 21:29:51 -0300

    Adding MapGet and MapPost
    adding Get and Post to the Entity

---
+++ MinimalAPI/Program.cs
@@ -0,0 +1,56 @@
+using Microsoft.EntityFrameworkCore;
+using MinimalAPI.Data;
+using MinimalAPI.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<MinimalContextDb>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.MapGet("/fornecedor", async(
+    MinimalContextDb context) =>
+    await context.Fornecedores.ToListAsync())
+    .WithName("GetFornecedor")
+    .WithTags("Fornecedor"); ;
+
+app.MapGet("/fornecedor/{id}", async (
+    Guid id, 
+    MinimalContextDb context) =>
+    await context.Fornecedores.FindAsync(id)
+        is Fornecedor fornecedor
+            ? Results.Ok(fornecedor)
+            : Results.NotFound())
+    .Produces<Fornecedor>(StatusCodes.Status200OK)
+    .Produces<Fornecedor>(StatusCodes.Status404NotFound)
+    .WithName("GetFornecedorPorId")
+    .WithTags("Fornecedor"); ;
+
+app.MapPost("/fornecedor", async (
+    MinimalContextDb context,
+    Fornecedor fornecedor) =>
+{
+    context.Fornecedores.Add(fornecedor);
+    var result = await context.SaveChangesAsync();
+})
+    .ProducesValidationProblem()
+    .Produces<Fornecedor>(StatusCodes.Status201Created)
+    .Produces(StatusCodes.Status400BadRequest)
+    .WithName("PostFornecedor")
+    .WithTags("Fornecedor"); 
+
+app.Run();
