5a98a3815f54ce8e7a240af91edab7e01a394f15
Jonathan Daniel Loth <ninoloth@gmail.com>
2022-03-28 21:53:14 -0300

    add PUT and DELETE methods

--- MinimalAPI/Program.cs
+++ MinimalAPI/Program.cs
@@ -61,6 +61,53 @@ app.MapPost("/fornecedor", async (
     .Produces<Fornecedor>(StatusCodes.Status201Created)
     .Produces(StatusCodes.Status400BadRequest)
     .WithName("PostFornecedor")
-    .WithTags("Fornecedor"); 
+    .WithTags("Fornecedor");
+
+app.MapPut("/fornecedor/{id}", async (
+        Guid id,
+        MinimalContextDb context,
+        Fornecedor fornecedor) =>
+{
+    var fornecedorBanco = await context.Fornecedores.AsNoTracking<Fornecedor>()
+                                                    .FirstOrDefaultAsync(f => f.Id == id);
+
+    if (fornecedorBanco == null) return Results.NotFound();
+
+    if (!MiniValidator.TryValidate(fornecedor, out var errors))
+        return Results.ValidationProblem(errors);
+
+    context.Fornecedores.Update(fornecedor);
+    var result = await context.SaveChangesAsync();
+
+    return result > 0
+        ? Results.NoContent()
+        : Results.BadRequest("Houve um problema ao salvar o registro");
+
+}).ProducesValidationProblem()
+    .Produces(StatusCodes.Status204NoContent)
+    .Produces(StatusCodes.Status400BadRequest)
+    .WithName("PutFornecedor")
+    .WithTags("Fornecedor");
+
+app.MapDelete("/fornecedor/{id}", async (
+        Guid id,
+        MinimalContextDb context) =>
+{
+    var fornecedor = await context.Fornecedores.FindAsync(id);
+    if (fornecedor == null) return Results.NotFound();
+
+    context.Fornecedores.Remove(fornecedor);
+    var result = await context.SaveChangesAsync();
+
+    return result > 0
+        ? Results.NoContent()
+        : Results.BadRequest("Houve um problema ao salvar o registro");
+
+}).Produces(StatusCodes.Status400BadRequest)
+    .Produces(StatusCodes.Status204NoContent)
+    .Produces(StatusCodes.Status404NotFound)
+    .RequireAuthorization("ExcluirFornecedor")
+    .WithName("DeleteFornecedor")
+    .WithTags("Fornecedor");
 
 app.Run();
